275778d23e5426ebc4671fbf6bab53ec3ca3bd19
milena-kos <75163549+milena-kos@users.noreply.github.com>
2022-10-02 12:42:02 +0300

    more indepetintion

--- main.py
+++ main.py
@@ -30,47 +30,47 @@ def broadcast(message):
 
 def handle(client, address):
 	client.send(to_v3(levelcode.find_one()["code"]).encode("utf-8"))
-	while True:
-		try:
-			message = client.recv(256).decode("utf-8") # receive up to 256 bytes from client.
-			# ensure cooldown isnt bypassed
-			if time.time() - cooldowns.find_one({"ip": address[0]})["time"] >= 60 and len(message.split(".")) == 4:
-				cooldowns.replace_one({"ip": address[0]}, {"ip": address[0], "time": time.time()})
+    while True:
+        try:
+            message = client.recv(256).decode("utf-8") # receive up to 256 bytes from client.
+            # ensure cooldown isnt bypassed
+            if time.time() - cooldowns.find_one({"ip": address[0]})["time"] >= 60 and len(message.split(".")) == 4:
+                cooldowns.replace_one({"ip": address[0]}, {"ip": address[0], "time": time.time()})
 				
-				current = levelcode.find_one()["code"]
-				cells = current.split(";")[4].split(",")
-				result = "V1;75;75;;"
+                current = levelcode.find_one()["code"]
+                cells = current.split(";")[4].split(",")
+                result = "V1;75;75;;"
 				
-				for i in cells:
-					# N means "remove cell"
-					if i[0] != "N":
-						if i.split(".")[-2:] == message.split(".")[-2:]:
-							i = message
-						result = result + message + ","
+                for i in cells:
+                    # N means "remove cell"
+                    if i[0] != "N":
+                        if i.split(".")[-2:] == message.split(".")[-2:]:
+                            i = message
+                        result = result + message + ","
 				
-				result = result[:-1] + ";;"
+                result = result[:-1] + ";;"
 				
-				levelcode.replace_one({"code": current}, result)
-				broadcast(message)
-		except ConnectionResetError:
-			print(f"Disconnected with {str(client)}!")
-			clients.remove(client)
-			client.close()
-			break
+                levelcode.replace_one({"code": current}, result)
+                broadcast(message)
+        except ConnectionResetError:
+            print(f"Disconnected with {str(client)}!")
+            clients.remove(client)
+            client.close()
+            break
 
 def receive():
     while True:
 		# accept anyone
-		client, address = server.accept()
+        client, address = server.accept()
         print(f"Connected with {address}, {str(client)}!")
 		
-		if not cooldowns.find_one({"ip": address[0]}):
-			cooldowns.insert_one({"ip": address[0], "time": 0})
+        if not cooldowns.find_one({"ip": address[0]}):
+            cooldowns.insert_one({"ip": address[0], "time": 0})
 
-		clients.append(client)
+        clients.append(client)
 
         print("stargint a threat")
-		thread = threading.Thread(target=handle, args=(client, address,))
-		thread.start()
+        thread = threading.Thread(target=handle, args=(client, address,))
+        thread.start()
 
 receive()
