4d6fd168c0daabb36d8199c9519cbaed6d57cf1f
Milenakos <business@milenakos.tk>
2023-03-22 11:57:35 +0000

    probably final change

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# amongus
 import socket, threading, time, catbase
+
+PORT = 30128
 
 db = catbase.CatDB("db.json", none=0)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 30128))
+server.bind(('', PORT))
 
 server.listen()
 
@@ -31,7 +32,7 @@ def handle(client, address):
             message = client.recv(256).decode("ascii") # receive up to 256 bytes from client.
             if message == "heartbeat":
                 if responses[address[0]] != []:
-                    client.send(responses[address[0]][0])
+                    client.send(responses[address[0]][0].encode("ascii"))
                     responses[address[0]].pop(0)
                 else:
                     client.send("heartbeat".encode("ascii"))
@@ -43,7 +44,8 @@ def handle(client, address):
 
                 current = db["levelcode"]
                 cells = current.split(";")[4].split(",")
-                result = "V1;75;75;;"
+                res = "V1;75;75;;"
+                result = ""
 
                 found = False
                 for i in cells:
@@ -52,14 +54,16 @@ def handle(client, address):
                         found = True
                         if i[0] == "N":
                             continue
-                    result = result + i + ","
+                    result += i + ","
 
                 if not found:
                     result += message + ","
+                    
+                if result[0] == ",": result = result[1:]
 
-                result = result[:-1] + ";;"
+                res += result[:-1] + ";;"
 
-                db["levelcode"] = result
+                db["levelcode"] = res
                 db.commit()
                 client.send(message.encode("ascii"))
                 broadcast(message, address[0])
